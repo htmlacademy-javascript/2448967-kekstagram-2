70d3e2923a029d0694788896addd57ec37e303f5
ClaireFotina <clairefotina18.03@gmail.comgit config --global user.email clairefotina18.03@gmail.com>
2024-12-15 12:05:35 +0400

    делает форму

---
+++ js/effects.js
@@ -0,0 +1,59 @@
+import { DEFAULT_EFFECT, EffectsSetting } from "./constants.js";
+
+const effectsList = document.querySelector('.effects__list');
+const sliderContainer = document.querySelector('.effect-level__slider');
+const value = document.querySelector('.effect-level__value');
+const imageUploadPreview = document.querySelector('.img-upload__preview img');
+const sliderBlock = document.querySelector('.effect-level');
+
+let currentEffect = DEFAULT_EFFECT;
+
+noUiSlider.create(sliderContainer, {
+  range: {
+    min: 0,
+    max: 100,
+  },
+  start: 80,
+  step: 1,
+  connect: 'lower',
+})
+
+const render = () => {
+  const { style, units } = EffectsSetting[currentEffect];
+  imageUploadPreview.style.filter = `${style}(${value.value}${units})`;
+}
+
+const updateSlider = () => {
+  const { min, max, step } = EffectsSetting[currentEffect];
+  sliderContainer.noUiSlider.updateOptions({
+    range: {
+      min,
+      max,
+    },
+    step,
+    start: max,
+  }
+  )
+}
+
+sliderContainer.noUiSlider.on('update', () => {
+  value.value = sliderContainer.noUiSlider.get();
+  render();
+})
+
+export const reset = () => {
+  imageUploadPreview.style.filter = '';
+  sliderBlock.classList.add('hidden');
+}
+
+effectsList.addEventListener('change', ({ target }) => {
+  currentEffect = target.value;
+  if (currentEffect === DEFAULT_EFFECT) {
+    reset();
+  } else {
+    updateSlider();
+    sliderBlock.classList.remove('hidden');
+  }
+})
+
+reset()
---
+++ js/scale.js
@@ -0,0 +1,37 @@
+const SCALE_VALUE_MIN = 25;
+const SCALE_VALUE_MAX = 100;
+const SCALE_VALUE_STEP = 25;
+const SCALE_VALUE_DEFAULT = SCALE_VALUE_MAX;
+const FACTOR = 0.01;
+
+const scaleValue = document.querySelector('.scale__control--value');
+const imageUploadPreview = document.querySelector('.img-upload__preview img');
+const scalleSmaller = document.querySelector('.scale__control--smaller');
+const scalleBigger = document.querySelector('.scale__control--bigger');
+
+let currentScale = SCALE_VALUE_DEFAULT;
+
+const render = () => {
+  scaleValue.value = `${currentScale}%`;
+  imageUploadPreview.style.transform = `scale(${currentScale * FACTOR})`;
+}
+
+const onScalleSmallerClick = () => {
+  currentScale = currentScale > SCALE_VALUE_MIN ? currentScale - SCALE_VALUE_STEP : SCALE_VALUE_MIN;
+  render()
+}
+
+const onScalleBiggerClick = () => {
+  currentScale = currentScale < SCALE_VALUE_MAX ? currentScale + SCALE_VALUE_STEP : SCALE_VALUE_MAX;
+  render()
+}
+
+scalleSmaller.addEventListener('click', onScalleSmallerClick);
+scalleBigger.addEventListener('click', onScalleBiggerClick);
+
+export const reset = () => {
+  currentScale = SCALE_VALUE_DEFAULT;
+  render()
+}
+
+reset();
